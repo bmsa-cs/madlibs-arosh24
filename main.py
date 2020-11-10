7fc93d2e89329629c91fbc3cbef8e26b9de844a4
Andrew Roshong <aroshong24@biomedscienceacademy.org>
2020-11-10 19:12:35 +0000

    Finished all variables, all that's left is the main print statement.

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Andrew Roshong
+Period/Core: 7
 
 
 """
+"""
+[name] went to the store to get some [noun1] .
+When [name] stepped inside of the [adjective1] store, they notice a [adjective2] [noun2] standing in their way.
+They,[adverb1][verb1] past the [adjective2] [noun2] getting into the store.
+[name][adverb2][verb2 plural] down the aisle, rushing to find the spot where [noun1] is located...
+Finally you locate the [noun1] on the shelf next to some [adjective3][noun3].
+[name] [adverb3] [verb3 plural] out of the store, finally ready to go home.
+
+
+"""
+print("Let's play Silly Sentences!")
+name = input("Enter a name: ")
+noun1 = input("Enter a noun: ")
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter another adjective: ")
+noun2 = input("Enter a noun: ")
+adverb1 = input("Enter a adverb: ")
+verb1 = input("Enter a verb: ")
+adverb2 = input("Enter an adverb: ")
+verb2 = input("Enter a plural verb: ")
+adj3 = input("Enter an adjective: ")
+noun3 = input("Enter a plural noun: ")
+advern3 = input("Enter an adverb: ")
+verb3 = input("Enter a plural verb: ")
+
+
+
