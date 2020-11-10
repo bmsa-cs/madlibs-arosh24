3bb2b12908b045a3ced6ddf13da001060e4e1dfc
Andrew Roshong <aroshong24@biomedscienceacademy.org>
2020-11-10 19:45:30 +0000

    Finished print statement, pretty sure all done.

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ They,[adverb1][verb1] past the [adjective2] [noun2] getting into the store.
 Finally you locate the [noun1] on the shelf next to some [adjective3][noun3].
 [name] [adverb3] [verb3 plural] out of the store, finally ready to go home.
 
-
 """
-print("Let's play Silly Sentences!")
+
+print("Let's" + " " "play" + " " + "Silly" + " " + "Sentences!")
 name = input("Enter a name: ")
 noun1 = input("Enter a noun: ")
 adj1 = input("Enter an adjective: ")
@@ -27,8 +27,8 @@ adverb2 = input("Enter an adverb: ")
 verb2 = input("Enter a plural verb: ")
 adj3 = input("Enter an adjective: ")
 noun3 = input("Enter a plural noun: ")
-advern3 = input("Enter an adverb: ")
+adverb3 = input("Enter an adverb: ")
 verb3 = input("Enter a plural verb: ")
 
-
+print(f"{name} went to the store to get some {noun1}.\nWhen {name} stepped inside of the {adj1} store they notice a {adj2} {noun2} standing in their way.\nThey {adverb1} {verb1} past the {adj2} {noun2} barely managing to get into the store.\n{name} {adverb2} {verb2} down the aisle, rushing to find the spot where {noun1} is located...\nFinally {name} notices the {noun1} they were looking for on the shelf next to some {adj3} {noun3}\n{name} {adverb3} {verb3} out of the store, finally ready to go home. ")
 
